2f94be1adea27f24f2ae3194ab2dd782e12f1ea8
David Harris <davidharris006@gmail.com>
2019-07-25 21:24:46 -0700

    models started

---
+++ server/database/models/paid.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const paidSchema = new Schema({
+    userId: {type: String, required: true},
+    payerId: {type: String, required: true},
+    amount: {type: Number, required: true},
+    eventName: {type: String, required: true},
+    paid: {type: Boolean, require: true}
+})
+
+
+module.exports = paidSchema
