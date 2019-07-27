46addce7e5799e3446616a95f73aeffa0b54bcce
Cefalu Wave <luke.evan.graham@gmail.com>
2019-07-27 12:05:39 -0700

    added Upcoming Trips card to home

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import TotalBalanceCard from "../components/totalBalance";
+import TotalBalanceCard from "./totalBalance";
+import TripCardHome from "./tripCardHome";
 
 class Home extends Component {
   constructor() {
@@ -8,9 +9,13 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
-        <TotalBalanceCard userOwes={9} userIsOwed={8} />
-  
+      <div className="row">
+        <div className="col-md-4">
+          <TripCardHome tripName={"Denver"} />
+        </div>
+        <div className="col-md-4">
+          <TotalBalanceCard userOwes={9} userIsOwed={8} />
+        </div>
       </div>
     );
   }
