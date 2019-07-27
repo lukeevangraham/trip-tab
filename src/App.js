7284ddd955aebb2fb4fb2609ce18d181d98e042c
Cefalu Wave <luke.evan.graham@gmail.com>
2019-07-27 13:15:22 -0700

    reordered home page content

--- src/App.js
+++ src/App.js
@@ -57,8 +57,6 @@ class App extends Component {
         <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} currentUser={this.state.username}  />
         <div className="container">
           {/* greet user if logged in: */}
-          {this.state.loggedIn && <p>Join the party, {this.state.username}!</p>}
-          <Home2 currentUser={this.state.username} />
           {/* Routes to different components */}
           <Route exact path="/" component={Home} />
           <Route
@@ -66,6 +64,8 @@ class App extends Component {
             render={() => <LoginForm updateUser={this.updateUser} />}
           />
           <Route path="/signup" render={() => <Signup />} />
+          {this.state.loggedIn && <p>Join the party, {this.state.username}!</p>}
+          <Home2 currentUser={this.state.username} />
         </div>
       </div>
     );
