c4a20e9f45cd92b3f8c64f78af327a7ef3a30dcb
Luke Graham <luke.evan.graham@gmail.com>
2019-08-01 21:39:47 -0700

    Prettier sign up page (#29)
    * getting ready to switch to navbar update
    
    * trips and ledger disappear when logged out
    
    * looking better
    
    * form in progress
    
    * created a form option
    
    * removed duplicate Trip in app Router
    
    * better form formatting
    
    * tiny addition
    
    * prettied up the login form. 😚
    
    * signup page prettier

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -40,7 +40,7 @@ class Navbar extends Component {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="/">
             TripTab
           </a>
           <button
@@ -57,46 +57,46 @@ class Navbar extends Component {
 
           <div className="navbar-collapse collapse" id="navbarColor02">
             <ul className="navbar-nav mr-auto m-0">
-              <li className="nav-item m-0">
-                <Link
-                  to="/"
-                  className={
-                    window.location.pathname === "/"
-                      ? "nav-link active"
-                      : "nav-link"
-                  }
-                >
-                  Home <span className="sr-only">(current)</span>
-                </Link>
-              </li>
-
               {loggedIn ? (
                 <div className="navbar-nav mr-auto m-0">
                   <li className="nav-item m-0">
                     <Link
-                      to="/trips"
+                      to="/"
                       className={
-                        window.location.pathname === "/trips"
-                          ? "nav-link active non-underline-link"
-                          : "nav-link non-underline-link"
+                        window.location.pathname === "/"
+                          ? "nav-link active"
+                          : "nav-link"
                       }
                     >
-                      Trips
+                      Home <span className="sr-only">(current)</span>
                     </Link>
                   </li>
-                  <li className="nav-item m-0">
-                    <Link
-                      to="/ledger"
-                      className={
-                        window.location.pathname === "/ledger"
-                          ? "nav-link active non-underline-link"
-                          : "nav-link non-underline-link"
-                      }
-                    >
-                      Ledger
-                    </Link>
-                  </li>
-                </div>
+
+                    <li className="nav-item m-0">
+                      <Link
+                        to="/trips"
+                        className={
+                          window.location.pathname === "/trips"
+                            ? "nav-link active non-underline-link"
+                            : "nav-link non-underline-link"
+                        }
+                      >
+                        Trips
+                      </Link>
+                    </li>
+                    <li className="nav-item m-0">
+                      <Link
+                        to="/ledger"
+                        className={
+                          window.location.pathname === "/ledger"
+                            ? "nav-link active non-underline-link"
+                            : "nav-link non-underline-link"
+                        }
+                      >
+                        Ledger
+                      </Link>
+                    </li>
+                  </div>
               ) : (
                 ""
               )}
@@ -104,41 +104,44 @@ class Navbar extends Component {
 
             {loggedIn ? (
               <ul className="navbar-nav m-0">
-              <li className="nav-item dropdown ml-auto">
-                <Link
-                  style={{ color: "rgba(255,255,255,0.5)" }}
-                  className="nav-link dropdown-toggle text-right"
-                  to="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  {currentUser}
-                </Link>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li className="nav-item dropdown ml-auto">
                   <Link
                     style={{ color: "rgba(255,255,255,0.5)" }}
-                    className="dropdown-item"
+                    className="nav-link dropdown-toggle text-right"
                     to="#"
-                    onClick={this.logout}
+                    id="navbarDropdown"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
                   >
-                    Logout
+                    {currentUser}
                   </Link>
-                </div>
-              </li>
+                  <div
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <Link
+                      style={{ color: "rgba(255,255,255,0.5)" }}
+                      className="dropdown-item"
+                      to="#"
+                      onClick={this.logout}
+                    >
+                      Logout
+                    </Link>
+                  </div>
+                </li>
               </ul>
             ) : (
               <section className="navbar-section">
                 <Link to="/" className="btn btn-link text-secondary">
-                  <span className="text-secondary">home</span>
+                  <span className="text-secondary">Home</span>
                 </Link>
                 <Link to="/login" className="btn btn-link text-secondary">
-                  <span className="text-secondary">login</span>
+                  <span className="text-secondary">Login</span>
                 </Link>
                 <Link to="/signup" className="btn btn-link">
-                  <span className="text-secondary">sign up</span>
+                  <span className="text-secondary">Sign up</span>
                 </Link>
               </section>
             )}
