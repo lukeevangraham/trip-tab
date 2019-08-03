73bc841d7911cafd312a818a3b1eb8a40701d6d4
ajayraj <ajayraj98@gmail.com>
2019-08-02 23:45:17 -0700

    fresh new app :boom:

---
+++ client/src/components/home2.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+function Home2(props){
+    return(
+        <h2>{props.currentUser} is the current user</h2>
+    )
+}
+
+export default Home2;
---
+++ database/models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Owed: require("./owed"),
+  Paid: require("./paid"),
+  Event: require("./event")
+};
