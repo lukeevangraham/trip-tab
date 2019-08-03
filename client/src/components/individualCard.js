79668aa0a7df8819ce216a24be40ffd2fa741a30
Cefalu Wave <luke.evan.graham@gmail.com>
2019-08-03 12:16:49 -0700

    table ledger ready

--- client/src/components/individualCard.js
+++ client/src/components/individualCard.js
@@ -8,22 +8,16 @@ const styles = {
 
 function Individualcard(props) {
     return (
-        <div className={"card text-white bg-" + props.color} style={styles.cardStyle}>
-            <div className="row">
-                <div className="col-md-1">
-                    {/* <img src={props.img} alt="Image here" /> */}
-                    <i class="p-2 fas fa-user" />
-                </div>
-                <div className="col-md-4 p-2">
-                     {props.username}
-                </div>
-                <div className="col-md-3" />
-                <div className="col-md-4 p-2">
-                    Amount: ${props.amount}
 
-                </div>
-            </div>
-        </div>
+            <tr className={"text-white table-" + props.color}>
+                <th scope="row"><i class="p-2 fas fa-user" /></th>
+                <td className="align-middle">{props.username}</td>
+                <td className="align-middle">${props.amount}</td>
+            </tr>
+
+
+
+
     );
 }
 
