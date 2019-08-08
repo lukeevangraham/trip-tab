1c589e5d1de9e3f762554ae12a2717f3678b6c94
Cefalu Wave <luke.evan.graham@gmail.com>
2019-08-07 23:03:06 -0700

    better ledger buttons

--- client/src/components/individualCard.js
+++ client/src/components/individualCard.js
@@ -10,11 +10,15 @@ function Individualcard(props) {
     console.log(props)
     return (
             <tr className={"text-white table-" + props.color}>
-                <th scope="row"><i class="pl-2 fas fa-user" /></th>
+                {/* <th scope="row"><i class="pl-2 fas fa-user" /></th> */}
                 <td className="align-middle">{props.payee}</td>
                 <td className="align-middle">{props.eventName}</td>
                 <td className="align-middle">${props.amount.toFixed(2)}</td>
-                <td><button onClick={() => props.onClick(props.eventId, props.payee, props.eventName, props.username, props.amount)}>Pay</button></td>
+                {props.payee ? (
+                <td><button className="btn btn-dark text-light btn-sm" onClick={() => props.onClick(props.eventId, props.payee, props.eventName, props.username, props.amount)}>Pay</button></td>
+                ) : (
+                    ""
+                )}
             </tr>
     );
 }
