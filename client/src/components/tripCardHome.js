79c800a71fad4df8ba4184af5ab42f28f63ab3c9
Cefalu Wave <luke.evan.graham@gmail.com>
2019-08-08 00:04:31 -0700

    added buttons to home and renamed Trips Paid to Event Payoff

--- client/src/components/tripCardHome.js
+++ client/src/components/tripCardHome.js
@@ -1,22 +1,48 @@
 import React from "react";
-import "./totalBalance.css"
+import "./totalBalance.css";
+import { Route, Link } from "react-router-dom";
 
 function TripCardHome(props) {
-    return (
-        <div className="card text-white border-warning bg-warning  mb-3 rounded card-parent">
-            {/* <div className="card-header text-left">Upcoming Trips</div> */}
-            <div className="card-body text-left">
-                <h4 className="card-title mb-1">Upcoming trips</h4>
-                <p className="card-text mt-3 pl-3 align-top ">{props.tripName}:  <button
-                    type="button"
-                    className="btn btn-warning bg-secondary text-white border-warning btn-circle btn-xl text-center"
-                >
-                    Add<br />Expense
-        </button></p>
-
-            </div>
+  return (
+    <div className="card text-white border-warning bg-warning  mb-3 rounded card-parent">
+      {/* <div className="card-header text-left">Upcoming Trips</div> */}
+      <div className="card-body text-left">
+        <h4 className="card-title mb-1">What to do?</h4>
+        <div className="mx-auto">
+          <Link to="/trips">
+            <button
+              type="button"
+              className="btn btn-warning bg-secondary text-white border-warning btn-circle btn-xl text-center mr-3"
+            >
+              Create
+              <br />
+              Event
+            </button>
+          </Link>
+          <Link to="/tripsPaid">
+          <button
+            type="button"
+            className="btn btn-warning bg-secondary text-white border-warning btn-circle btn-xl text-center mr-3"
+          >
+            Payoff
+            <br />
+            Event
+          </button>
+          </Link>
+          <Link to="/ledger">
+          <button
+            type="button"
+            className="btn btn-warning bg-secondary text-white border-warning btn-circle btn-xl text-center mr-3"
+          >
+            View
+            <br />
+            Ledger
+          </button>
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default TripCardHome
+export default TripCardHome;
