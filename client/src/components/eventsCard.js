16e3394d6fea385af788c30776d278d6d61ebc12
Cefalu Wave <luke.evan.graham@gmail.com>
2019-08-08 17:45:54 -0700

    events page matches ledger page

--- client/src/components/eventsCard.js
+++ client/src/components/eventsCard.js
@@ -1,38 +1,25 @@
 import React from "react";
 import axios from "axios";
-import { Table } from 'reactstrap';
-import "../../../client/src/events.css"
+import { Table } from "reactstrap";
+import "../../../client/src/events.css";
 
-let participants = []
+let participants = [];
 
 function eventCard(props) {
-    return (
-        <div className="container">
-            <Table className={"table table-hover" + props.color}>
-                <tbody>
-                    <tr className={"text-white table-"}>
-                        <th scope="row">
-                            <ol>
-                                {props.participants.map(user => {
-                                    console.log(user);
-                                    return (<li> {user} </li>)
-                                })}
-                            </ol>
-                        </th>
-                        {/* <td className="text-center">
-                            <p className="lead">{props.username}</p>
-                        </td> */}
-                        <td>
-                        <p className="lead">{props.eventname}</p>
-                        </td>
-                        <td>
-                        <p className="lead">${props.amount}</p>
-                        </td>
-                    </tr>
-                </tbody>
-            </Table>
-        </div>
-     
+  return (
+      <tr className={"text-white table-" + props.color}>
+        <td className="align-top">
+          <ol>
+            {props.participants.map(user => {
+              console.log(user);
+              return <li> {user} </li>;
+            })}
+          </ol>
+        </td>
+        <td className="align-top">{props.eventname}</td>
+        <td className="align-top">${props.amount}
+        </td>
+      </tr>
   );
 }
 
